6dab860e4c650632c1c45835ee4d5a8809ffd9ba
hiroki <>
2018-06-15 12:23:46 +0900

    Fitting用の基本的なexponential fitting関数を作成

--- DummyData.m
+++ DummyData.m
@@ -1,4 +1,4 @@
-clear; close all;
+clear; clf;
 %function [t,y] = DummyData
 
 t = -20:100;
@@ -10,14 +10,15 @@ y = 0.5 * exp(-t/tau1) + 0.5 * exp(-t/tau2);
 y(t<=0) = 0;
 
 % instrumental function
+t2 = -10:10;
 c = 0; lw = 2;
-irf = 1/sqrt(pi*lw)*exp(-(t-c).^2/lw^2);
+irf = 1/sqrt(pi*lw)*exp(-(t2-c).^2/lw^2);
 
 % convoluted
-z = conv(irf, y);
+z = conv(y, irf,'same');
 
 % plotting
-semilogy(t,y,t,irf);
+plot(t,y,t2,irf);
 ylim([1e-2 max(y)])
 hold on;
-plot(z);
+plot(t,z);
