55659e00efa5b432e11d0127c520cf37204a8fad
hiroki <>
2018-06-15 21:05:16 +0900

    アルゴリズムを改善。横軸のシフトについて、可能性のある全てのパターンについてフィッティングを行い最適なシフトを計算するように調整

--- TestScript.m
+++ TestScript.m
@@ -17,7 +17,7 @@ irf = 1/sqrt(pi*lw)*exp(-(t2-c).^2/lw^2);
 
 % convoluted
 z = conv(y, irf,'same');
-z = addnoise(z,1000000);
+z = addnoise(z,100);
 
 
 % plotting
@@ -71,14 +71,19 @@ pos = I(1);
 
 
 base = @(tau) cv(tau,irf,pos,t);
+z = base(taus);
+rd = zeros(size(z,3));
+for k = 1:size(z,3)
+    A = z(:,:,k).'\expdata.';
+    A(A<0) = 0;
+    rd(k) = sum(abs(y - A.' * z(:,:,k)));
+end
+[~,I] = min(rd);
 
-A = base(taus).'\expdata.';
-A(A<0) = 0;
-%plot(t,y,t,A.' * base(taus))
-amp = rand(size(A))*max(y);
-lb = zeros(size(A));
-%A = lsqlin(base(taus).',y.',[],[],[],[],lb,[]);
-simy = A.'*base(taus);
+z2 = squeeze(z(:,:,I(1)));
+A = z2.'\expdata.';
+
+simy = A.'*z2;
 rasid = y - simy;
 
 end
@@ -95,9 +100,13 @@ for k = 1:size(pe,1)
 end
 
 [~,I] = max(a,[],2);
-pos = I(1);% peak position of simulation
-tshift = pos - datapeak + 1;
-%[1+tshift, (tshift+length(t))]
-z = a(:,1+tshift:(tshift+length(t)));
+
+z = zeros([size(a,1), length(t), length(I)]);
+for k = 1:length(I)
+    pos = I(k);% peak position of simulation    
+    tshift = pos - datapeak + 1;
+    %[1+tshift, (tshift+length(t))]
+    z(:,:,k) = a(:,1+tshift:(tshift+length(t)));
+end
 
 end
